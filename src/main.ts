fbe5247e52737455421faa66091b1c6e28dedf4e
dojoohwan <ehwnghks@gmail.com>
2024-06-05 17:10:18 +0900

    postgresql docker 실행 테스트 :                                                                                                                            HUP ✘  17:09:45 - compose.yml 에서 env_file 로 사용할 env파일 명 지정 가능 - Dockerfile에서 지정하는 'ENV NODE_ENV prod' 나 package.json 에서 "start:dev": "NODE_ENV=dev nest start --watch", 와 같이 NODE_ENV를 지정해서   API 서버에서 사용할 env파일을 결정하는게 docker로 사용시 잘 적용 되나 확인 해볼 예정

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 5000;
   await app.listen(port);
+  const configService = app.get(ConfigService);
+  const target = configService.get<string>('GOOGLE_TARGET_URL');
   console.log(`Application is running on port: ${port}`);
+  console.log(`Application is running ${process.env.GOOGLE_TARGET_URL}`);
+  console.log(`Application is running ${target}`);
 }
 bootstrap();
