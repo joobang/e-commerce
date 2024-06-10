221a9a76fbc0787bd97a786cca68a90b884fb793
dojoohwan <ehwnghks@gmail.com>
2024-06-10 14:21:51 +0900

    prisma 설치 및 user 테이블 스키마 설정 및 테이블 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -12,6 +15,7 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from "@nestjs/config";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger('main');
   const app = await NestFactory.create(AppModule);
   const port = 5000;
   await app.listen(port);
   const configService = app.get(ConfigService);
   const target = configService.get<string>('GOOGLE_TARGET_URL');
-  console.log(`Application is running on port: ${port}`);
-  console.log(`Application is running ${process.env.GOOGLE_TARGET_URL}`);
-  console.log(`Application is running ${target}`);
+  logger.log(`Application is running on port: ${port}`);
+  // console.log(`Application is running ${process.env.GOOGLE_TARGET_URL}`);
+  // console.log(`Application is running ${target}`);
 }
 bootstrap();
