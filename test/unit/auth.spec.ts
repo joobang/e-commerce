38a260bc4f1ac998225287c4df7a063815282f52
dojoohwan <ehwnghks@gmail.com>
2024-06-13 16:32:33 +0900

    auth/controller GET oauth2/redirect/google 유닛 테스트 추가

--- test/unit/auth.spec.ts
+++ test/unit/auth.spec.ts
@@ -5,15 +5,18 @@ import { PrismaService } from '../../src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { GoogleAuthGuard } from '../../src/auth/guard/google.guard';
-import { ExecutionContext, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { GoogleStrategy } from '../../src/auth/strategy/google.strategy';
-import * as passport from 'passport';
-import { NextFunction } from 'express';
+import { GoogleRequest } from '../../src/auth/dto/google.user';
+import { Request, Response } from 'express';
+import { INestApplication } from '@nestjs/common';
 
 describe('AuthController (unit)', () => {
   let app: INestApplication;
   let authController: AuthController;
+  let authService: AuthService;
+  let jwtService: JwtService;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -29,6 +32,10 @@ describe('AuthController (unit)', () => {
     }).compile();
 
     authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
+    configService = module.get<ConfigService>(ConfigService);
+
     app = module.createNestApplication();
     await app.init();
   });
@@ -44,6 +51,49 @@ describe('AuthController (unit)', () => {
 
     // response 헤더의 location이 Google 로그인 페이지로 리다이렉션되는지 확인
     expect(response.headers.location).toContain('accounts.google.com');
+  });
+
+  it('oauth2/redirect/google : googleAuthRedirect', async () => {
+    const mockReq = {
+      user: {
+        email: 'test@example.com',
+        username: 'Test User',
+        photo: 'https://test.com/photo.jpg',
+      },
+    } as Request & GoogleRequest;
+
+    const mockRes = {
+      redirect: jest.fn(),
+    } as unknown as Response;
+
+    const user = {
+      id: 1,
+      name: 'Test User',
+      email: 'test@example.com',
+      profile_image: 'https://test.com/photo.jpg',
+      password: 'hashedpassword',
+      user_desc: null,
+    };
+
+    jest.spyOn(authService, 'getUserByEmail').mockResolvedValueOnce(null);
+    jest.spyOn(authService, 'signUp').mockResolvedValueOnce(user);
+    jest.spyOn(jwtService, 'sign').mockReturnValue('mockToken');
+    jest.spyOn(configService, 'get').mockReturnValue('https://example.com');
+
+    await authController.googleAuthRedirect(mockReq, mockRes);
+
+    expect(authService.getUserByEmail).toHaveBeenCalledWith('test@example.com');
+    expect(authService.signUp).toHaveBeenCalledWith({
+      name: 'Test User',
+      email: 'test@example.com',
+      profile_image: 'https://test.com/photo.jpg',
+      password: '1234',
+    });
+    expect(jwtService.sign).toHaveBeenCalledWith({ email: 'test@example.com' });
+    expect(configService.get).toHaveBeenCalledWith('GOOGLE_TARGET_URL');
+    expect(mockRes.redirect).toHaveBeenCalledWith(
+      'https://example.com?token=token',
+    );
   });
 
   afterAll(async () => {
