b1037e3a3d9ba0372666e900409b68f415c57d41
dojoohwan <ehwnghks@gmail.com>
2024-06-13 10:45:56 +0900

    유닛 테스트, e2e 테스트 둘 다 작성해보기 : auth.spec.ts -> 유닛테스트 / auth.e2e-spec.ts -> e2e 테스트

--- test/unit/auth.spec.ts
+++ test/unit/auth.spec.ts
@@ -1,73 +1,23 @@
+import { AuthController } from '../../src/auth/auth.controller';
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from '../../src/auth/auth.service';
+import { PrismaService } from '../../src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 
-import * as jwt from 'jsonwebtoken';
-import * as request from 'supertest';
-
-import { CanActivate, INestApplication } from '@nestjs/common';
-import { AuthController } from '../../src/auth/auth.controller';
-import { AuthService } from '../../src/auth/auth.service';
-import { PrismaService } from '../../src/prisma/prisma.service';
-import { GoogleAuthGuard } from "../../src/auth/guard/google.guard";
-
-describe('AuthController', () => {
-  let app: INestApplication;
-  let authController: AuthController;
-  let authService: AuthService;
-  let jwtService: JwtService;
-  let configService: ConfigService;
-  let prismaService: PrismaService;
+describe('AuthController (unit)', () => {
+  let controller: AuthController;
 
   beforeEach(async () => {
-    const mockGoogleGuard: CanActivate = { canActivate: jest.fn(() => true) };
-
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [
-        AuthService,
-        JwtService,
-        ConfigService,
-        {
-          provide: PrismaService,
-          useValue: {
-            user: {
-              create: jest.fn(),
-              findUnique: jest.fn(),
-            },
-          },
-        },
-      ],
-    })
-      .overrideGuard(GoogleAuthGuard)
-      .useValue(mockGoogleGuard)
-      .compile();
+      providers: [AuthService, PrismaService, JwtService, ConfigService],
+    }).compile();
 
-    app = module.createNestApplication();
-    await app.init();
-
-    authController = module.get<AuthController>(AuthController);
-    authService = module.get<AuthService>(AuthService);
-    jwtService = module.get<JwtService>(JwtService);
-    configService = module.get<ConfigService>(ConfigService);
-    prismaService = module.get<PrismaService>(PrismaService);
-  });
-
-  afterEach(async () => {
-    await app.close();
+    controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
-    expect(authController).toBeDefined();
-  });
-
-  describe('GET /auth/login/google', () => {
-    it('should use GoogleAuthGuard and redirect to Google OAuth', async () => {
-      const response = await request(app.getHttpServer())
-        .get('/auth/login/google')
-        .expect(200); // Expecting a redirect status code
-
-      //expect(response.header['location']).toMatch(/^https:\/\/google\.com/); // Expecting redirection to Google OAuth URL
-    });
+    expect(controller).toBeDefined();
   });
 });
