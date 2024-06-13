c92398ef4519b5b87b12d952d57d33229534104e
dojoohwan <ehwnghks@gmail.com>
2024-06-13 11:23:42 +0900

    auth/controller GET login/google 유닛테스트 추가 및 확인

--- test/unit/auth.spec.ts
+++ test/unit/auth.spec.ts
@@ -4,20 +4,61 @@ import { AuthService } from '../../src/auth/auth.service';
 import { PrismaService } from '../../src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { GoogleAuthGuard } from '../../src/auth/guard/google.guard';
+import { ExecutionContext, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { GoogleStrategy } from '../../src/auth/strategy/google.strategy';
 
 describe('AuthController (unit)', () => {
-  let controller: AuthController;
+  let app: INestApplication;
+  let authController: AuthController;
+  let googleAuthGuard: GoogleAuthGuard;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, PrismaService, JwtService, ConfigService],
+      providers: [
+        AuthService,
+        PrismaService,
+        JwtService,
+        ConfigService,
+        GoogleStrategy,
+        {
+          provide: GoogleAuthGuard,
+          useValue: {
+            canActivate: jest.fn((context: ExecutionContext) => {
+              return true;
+            }),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    googleAuthGuard = module.get<GoogleAuthGuard>(GoogleAuthGuard);
+
+    app = module.createNestApplication();
+    await app.init();
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  it('login/google : googleAuth', async () => {
+    const canActivateSpy = jest.spyOn(googleAuthGuard, 'canActivate');
+
+    //await authController.googleAuth();
+
+    const response = await request(app.getHttpServer())
+      .get('/auth/login/google')
+      .expect(302);
+
+    expect(canActivateSpy).toHaveBeenCalled();
+    //expect(response.headers.location).toBe('/oauth2/redirect/google');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
