dc0ed2b7905cf4a2942f949fa4ef263235ce201c
dojoohwan <ehwnghks@gmail.com>
2024-06-13 09:39:42 +0900

    테스트 파일을 위한 디렉토리 생성 및 파일 생성/ jest 옵션 변경 rootDir : src -> test

--- test/unit/auth.spec.ts
+++ test/unit/auth.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import { PrismaService } from '../prisma/prisma.service';
-import { GoogleAuthGuard } from './guard/google.guard';
-import { Request, Response } from 'express';
+
+import * as jwt from 'jsonwebtoken';
+import * as request from 'supertest';
+
+import { CanActivate, INestApplication } from '@nestjs/common';
+import { AuthController } from '../../src/auth/auth.controller';
+import { AuthService } from '../../src/auth/auth.service';
+import { PrismaService } from '../../src/prisma/prisma.service';
+import { GoogleAuthGuard } from "../../src/auth/guard/google.guard";
 
 describe('AuthController', () => {
+  let app: INestApplication;
   let authController: AuthController;
   let authService: AuthService;
   let jwtService: JwtService;
@@ -15,6 +20,8 @@ describe('AuthController', () => {
   let prismaService: PrismaService;
 
   beforeEach(async () => {
+    const mockGoogleGuard: CanActivate = { canActivate: jest.fn(() => true) };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [
@@ -33,8 +40,11 @@ describe('AuthController', () => {
       ],
     })
       .overrideGuard(GoogleAuthGuard)
-      .useValue({})
+      .useValue(mockGoogleGuard)
       .compile();
+
+    app = module.createNestApplication();
+    await app.init();
 
     authController = module.get<AuthController>(AuthController);
     authService = module.get<AuthService>(AuthService);
@@ -43,11 +53,21 @@ describe('AuthController', () => {
     prismaService = module.get<PrismaService>(PrismaService);
   });
 
+  afterEach(async () => {
+    await app.close();
+  });
+
   it('should be defined', () => {
     expect(authController).toBeDefined();
   });
 
-  describe('googleAuthRedirect', () => {
+  describe('GET /auth/login/google', () => {
+    it('should use GoogleAuthGuard and redirect to Google OAuth', async () => {
+      const response = await request(app.getHttpServer())
+        .get('/auth/login/google')
+        .expect(200); // Expecting a redirect status code
 
+      //expect(response.header['location']).toMatch(/^https:\/\/google\.com/); // Expecting redirection to Google OAuth URL
+    });
   });
 });
